c00f6ccd0e097e90044490575be2a95737f136a1
Jim Boyd <jimb@cabosoft.com>
2013-10-27 18:30:43 -0600

    ARC support. Fixed issue with all day event values.

--- iCalObjCSDK/NSDate+CGICalendar.m
+++ iCalObjCSDK/NSDate+CGICalendar.m
@@ -7,42 +7,83 @@
 
 #import "NSDate+CGICalendar.h"
 
-#define CGNSDATE_ICALENDAR_DATETMEFORMAT @"YYYYMMdd'T'kkmmss'Z'"
-#define CGNSDATE_ISO8601_DATETMEFORMAT @"YYYY-MM-dd kk:mm:ss"
+#define CGNSDATE_ICALENDAR_DATEFORMAT @"yyyyMMdd"
+#define CGNSDATE_ICALENDAR_DATETMEFORMAT @"yyyyMMdd'T'kkmmss'Z'"
+#define CGNSDATE_ISO8601_DATETMEFORMAT @"yyyy-MM-dd kk:mm:ss"
 
 @implementation NSDate(CGICalendar)
 
 + (id)dateWithICalendarString:(NSString *)aString
 {
-    NSDateFormatter *dateFormatter = [[[NSDateFormatter alloc] init] autorelease];
-    NSTimeZone *timeZone = [NSTimeZone timeZoneWithName:@"UTC"];
-	[dateFormatter setTimeZone:timeZone];
-    [dateFormatter setDateFormat:CGNSDATE_ICALENDAR_DATETMEFORMAT];
-    return [dateFormatter dateFromString:aString];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+	
+	if (aString.length > 8)
+	{
+		static NSDateFormatter* formatter = nil;
+		if (nil == formatter)
+		{
+			formatter = [[NSDateFormatter alloc] init];
+			NSTimeZone *timeZone = [NSTimeZone timeZoneWithName:@"UTC"];
+			[formatter setTimeZone:timeZone];
+			[formatter setDateFormat:CGNSDATE_ICALENDAR_DATETMEFORMAT];
+		}
+		
+		dateFormatter = formatter;
+	}
+	else
+	{
+		static NSDateFormatter* formatter = nil;
+		if (nil == formatter)
+		{
+			formatter = [[NSDateFormatter alloc] init];
+			[formatter setDateFormat:CGNSDATE_ICALENDAR_DATEFORMAT];
+		}
+		
+		dateFormatter = formatter;
+	}
+	
+    NSDate * result = [dateFormatter dateFromString:aString];
+	
+	return result;
 }
 
 + (id)dateWithICalendarISO8601:(NSString *)aString
 {
-    NSDateFormatter *dateFormatter = [[[NSDateFormatter alloc] init] autorelease];
-    [dateFormatter setTimeStyle:NSDateFormatterFullStyle];
-    [dateFormatter setDateFormat:CGNSDATE_ISO8601_DATETMEFORMAT];
+	static NSDateFormatter* dateFormatter = nil;
+	
+	if (nil == dateFormatter)
+	{
+		[dateFormatter setTimeStyle:NSDateFormatterFullStyle];
+		[dateFormatter setDateFormat:CGNSDATE_ISO8601_DATETMEFORMAT];
+	}
+	
     return [dateFormatter dateFromString:aString];
 }
 
 - (NSString *)descriptionICalendar
 {
-	NSDateFormatter *dateFormatter = [[[NSDateFormatter alloc] init] autorelease];
-    NSTimeZone *timeZone = [NSTimeZone timeZoneWithName:@"UTC"];
-	[dateFormatter setTimeZone:timeZone];
-	[dateFormatter setDateFormat:CGNSDATE_ICALENDAR_DATETMEFORMAT];
+	static NSDateFormatter* dateFormatter = nil;
+	
+	if (nil == dateFormatter)
+	{
+		NSTimeZone *timeZone = [NSTimeZone timeZoneWithName:@"UTC"];
+		[dateFormatter setTimeZone:timeZone];
+		[dateFormatter setDateFormat:CGNSDATE_ICALENDAR_DATETMEFORMAT];
+	}
+
 	return [dateFormatter stringFromDate:self];
 }
 
 - (NSString *)descriptionISO8601
 {
-	NSDateFormatter *dateFormatter = [[[NSDateFormatter alloc] init] autorelease];
-	[dateFormatter setLocale:[NSLocale systemLocale]];
-	[dateFormatter setDateFormat:CGNSDATE_ISO8601_DATETMEFORMAT];
+	static NSDateFormatter* dateFormatter = nil;
+	
+	if (nil == dateFormatter)
+	{
+		[dateFormatter setLocale:[NSLocale systemLocale]];
+		[dateFormatter setDateFormat:CGNSDATE_ISO8601_DATETMEFORMAT];
+	}
+	
 	return [dateFormatter stringFromDate:self];
 }
 
